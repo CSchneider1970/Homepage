1d7ee44a7605f17bd814f5d8fc1faf0bee535a46
Christoph Schneider <Christoph.Schneider@dci-student.org>
2025-03-07 17:49:58 +0100

    All Files reformated using Visual Studio Code

--- javascript.js
+++ javascript.js
@@ -1,9 +1,9 @@
-function enableDarkMode () {
+function enableDarkMode() {
   document.body.style.backgroundColor = "#101010";
   document.body.style.color = "#dddddd";
 }
 
-function disableDarkMode () {
+function disableDarkMode() {
   document.body.style.backgroundColor = "#f0f0f0";
   document.body.style.color = "#101010";
 }
